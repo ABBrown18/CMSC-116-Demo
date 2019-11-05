d244bfe4aa315cef4eb7cb4401a76bda91f40c50
nbreemscom <nbreemscom@gmail.com>
2019-11-05 00:28:51 -0600

    Update python_animation.py
    Added a high-speed purple ball

--- python_animation.py
+++ python_animation.py
@@ -12,7 +12,7 @@ class Ball():
         self.xspeed=random.randint(1,6)#user can give suitable speed
         self.yspeed=random.randint(5,10)#user can give suitable speed
 
-    def Move(self):
+    def move(self):
         canvas.move(self.shape,self.xspeed,self.yspeed)
         pos=canvas.coords(self.shape)
         if pos[3]>=500 or pos[1]<=0:
@@ -23,11 +23,15 @@ class Ball():
 redball=Ball("red",60)
 greenball=Ball("green",100)
 orangeball=Ball("orange",60)#here user can create another ball 
+purpleball=Ball("purple", 50)
+purpleball.xspeed=50
+purpleball.yspeed=50
 
 while True:
-    redball.Move()
-    greenball.Move()
-    orangeball.Move()
+    redball.move()
+    greenball.move()
+    orangeball.move()
+    purpleball.move()
     #if user create annother ball then user must call the move function inside while loop
     win.update()
     time.sleep(0.05)
