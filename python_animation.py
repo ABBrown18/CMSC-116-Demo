5c383a5e5c46f86d60d2c8dcb90c4aaf7a7f581f
nbreemscom <nbreemscom@gmail.com>
2019-11-05 00:26:13 -0600

    Create python_animation.py
    Copied and pasted code from online to start with.

---
+++ python_animation.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import time
+import random
+
+win=Tk()
+canvas=Canvas(win,width=400,height=500)
+canvas.pack()
+
+class Ball():
+    def __init__(self,color,size):    
+        self.shape=canvas.create_oval(10,10,size,size,fill=color)
+        self.xspeed=random.randint(1,6)#user can give suitable speed
+        self.yspeed=random.randint(5,10)#user can give suitable speed
+
+    def Move(self):
+        canvas.move(self.shape,self.xspeed,self.yspeed)
+        pos=canvas.coords(self.shape)
+        if pos[3]>=500 or pos[1]<=0:
+            self.yspeed=-self.yspeed
+        if pos[2]>=400 or pos[0]<=0:
+            self.xspeed=-self.xspeed
+            
+redball=Ball("red",60)
+greenball=Ball("green",100)
+orangeball=Ball("orange",60)#here user can create another ball 
+
+while True:
+    redball.Move()
+    greenball.Move()
+    orangeball.Move()
+    #if user create annother ball then user must call the move function inside while loop
+    win.update()
+    time.sleep(0.05)
